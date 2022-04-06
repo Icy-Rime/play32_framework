61b119e166ca8c5c27936240717b8db53d47d923
Dreagonmon <531486058@qq.com>
2022-04-06 20:28:28 +0800

    improve _boot.py

--- lib/_boot.py
+++ lib/_boot.py
@@ -10,6 +10,41 @@ except OSError:
 
     vfs = inisetup.setup()
 
+# main.py
+def main():
+    # >>>> config memory <<<<
+    import gc
+    _threshold = (gc.mem_free() * 60) // 100 # 60% gc auto collect
+    gc.threshold(_threshold)
+    print("gc threshold has been set to", _threshold)
+    del _threshold
+    # >>>> recovery mode <<<<
+    import hal_keypad
+    hal_keypad.init()
+    hal_keypad.clear_key_status([hal_keypad.KEY_A, hal_keypad.KEY_B])
+    __count = 0
+    for event in hal_keypad.get_key_event():
+        event_type, key = hal_keypad.parse_key_event(event)
+        if event_type == hal_keypad.EVENT_KEY_PRESS:
+            if key in [hal_keypad.KEY_A, hal_keypad.KEY_B]:
+                __count += 1
+    if __count >= 2:
+        # enter recovery mode
+        import updater
+        updater._on_enter_recovery_mode_()
+    # >>>> main <<<<
+    else:
+        del __count
+        try:
+            from play32sys import app
+            app._on_boot_()
+            pass
+        except Exception as e:
+            import usys, updater
+            usys.print_exception(e)
+            updater._on_enter_recovery_mode_()
+    print("==== End Main ====")
+
 import usys, esp, machine, micropython
 # framework_debug
 def _exist(pt):
@@ -17,51 +52,13 @@ def _exist(pt):
         return uos.stat(pt)
     except OSError:
         return False
-if _exist("/framework_debug"):
-    usys.path[:] = ['lib', '', '/lib', '/', '.frozen']
-    try:
-        import boot, main
-    except Exception as e:
-        usys.print_exception(e)
-    usys.exit(0)
-usys.path[:] = ['.frozen', 'lib', '', '/lib', '/']
-esp.osdebug(None)
-machine.freq(240000000)
-micropython.alloc_emergency_exception_buf(100)
-del bdev, _exist, uos, usys, esp, machine, micropython
-gc.collect()
-del gc
-
-# main.py
-# >>>> config memory <<<<
-import gc
-_threshold = (gc.mem_free() * 60) // 100 # 60% gc auto collect
-gc.threshold(_threshold)
-print("gc threshold has been set to", _threshold)
-del _threshold
-# >>>> recovery mode <<<<
-import hal_keypad
-hal_keypad.init()
-hal_keypad.clear_key_status([hal_keypad.KEY_A, hal_keypad.KEY_B])
-__count = 0
-for event in hal_keypad.get_key_event():
-    event_type, key = hal_keypad.parse_key_event(event)
-    if event_type == hal_keypad.EVENT_KEY_PRESS:
-        if key in [hal_keypad.KEY_A, hal_keypad.KEY_B]:
-            __count += 1
-if __count >= 2:
-    # enter recovery mode
-    import updater
-    updater._on_enter_recovery_mode_()
-# >>>> main <<<<
+if not _exist("/framework_debug"):
+    usys.path[:] = ['.frozen', 'lib', '', '/lib', '/']
+    esp.osdebug(None)
+    machine.freq(240000000)
+    micropython.alloc_emergency_exception_buf(100)
+    gc.collect()
+    main() # main function
 else:
-    del __count
-    try:
-        from play32sys import app
-        app._on_boot_()
-        pass
-    except Exception as e:
-        import usys, updater
-        usys.print_exception(e)
-        updater._on_enter_recovery_mode_()
-print("==== End Main ====")
+    usys.path[:] = ['lib', '', '/lib', '/', '.frozen']
+    # let micropython load boot.py and main.py
